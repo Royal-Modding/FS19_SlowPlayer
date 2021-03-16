ac97dec352f74ea89ed2711c5c9c71d3919f39dc
TyKonKet <matteoautieri@live.it>
2021-03-16 18:20:28 +0100

    1.0.0.0 RC1

--- src/slowPlayer.lua
+++ src/slowPlayer.lua
@@ -5,93 +5,24 @@
 ---@date 16/03/2021
 
 InitRoyalMod(Utils.getFilename("rmod/", g_currentModDirectory))
+InitRoyalUtility(Utils.getFilename("utility/", g_currentModDirectory))
 
+---@class SlowPlayer : RoyalMod
 SlowPlayer = RoyalMod.new(r_debug_r, false)
 
 function SlowPlayer:initialize()
+    Utility.overwrittenFunction(Player, "getDesiredSpeed", SlowPlayer.player_getDesiredSpeed)
 end
 
-function SlowPlayer:onMissionInitialize(baseDirectory, missionCollaborators)
-end
-
-function SlowPlayer:onSetMissionInfo(missionInfo, missionDynamicInfo)
-end
-
-function SlowPlayer:onLoad()
-end
-
-function SlowPlayer:onPreLoadMap(mapFile)
-end
-
-function SlowPlayer:onCreateStartPoint(startPointNode)
-end
-
-function SlowPlayer:onLoadMap(mapNode, mapFile)
-end
-
-function SlowPlayer:onPostLoadMap(mapNode, mapFile)
-end
-
-function SlowPlayer:onLoadSavegame(savegameDirectory, savegameIndex)
-end
-
-function SlowPlayer:onPreLoadVehicles(xmlFile, resetVehicles)
-end
-
-function SlowPlayer:onPreLoadItems(xmlFile)
-end
-
-function SlowPlayer:onPreLoadOnCreateLoadedObjects(xmlFile)
-end
-
-function SlowPlayer:onLoadFinished()
-end
-
-function SlowPlayer:onStartMission()
-end
-
-function SlowPlayer:onMissionStarted()
-end
-
-function SlowPlayer:onWriteStream(streamId)
-end
-
-function SlowPlayer:onReadStream(streamId)
-end
-
-function SlowPlayer:onUpdate(dt)
-end
-
-function SlowPlayer:onUpdateTick(dt)
-end
-
-function SlowPlayer:onWriteUpdateStream(streamId, connection, dirtyMask)
-end
-
-function SlowPlayer:onReadUpdateStream(streamId, timestamp, connection)
-end
-
-function SlowPlayer:onMouseEvent(posX, posY, isDown, isUp, button)
-end
-
-function SlowPlayer:onKeyEvent(unicode, sym, modifier, isDown)
-end
-
-function SlowPlayer:onDraw()
-end
-
-function SlowPlayer:onPreSaveSavegame(savegameDirectory, savegameIndex)
-end
-
-function SlowPlayer:onPostSaveSavegame(savegameDirectory, savegameIndex)
-end
-
-function SlowPlayer:onPreDeleteMap()
-end
-
-function SlowPlayer:onDeleteMap()
-end
-
-function SlowPlayer:onLoadHelpLine()
-    --return self.directory .. "gui/helpLine.xml"
+function SlowPlayer.player_getDesiredSpeed(player, superFunc, ...)
+    local speed = superFunc(player, ...)
+    SlowPlayer.dbgSpeed = speed
+    if player.isCarryingObject and speed > player.motionInformation.maxIdleSpeed then
+        speed = 1
+        if player.inputInformation.runAxis > 0 then
+            speed = 8
+        end
+    end
+    SlowPlayer.dbgOSpeed = speed
+    return speed
 end
