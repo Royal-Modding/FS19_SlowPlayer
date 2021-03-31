6ad46c3c630e29aff0a6a3bd33df057f417cd769
TyKonKet <matteoautieri@live.it>
2021-03-31 10:55:34 +0200

    small cleanup

--- src/slowPlayer.lua
+++ src/slowPlayer.lua
@@ -14,15 +14,17 @@ function SlowPlayer:initialize()
     Utility.overwrittenFunction(Player, "getDesiredSpeed", SlowPlayer.player_getDesiredSpeed)
 end
 
+---@param player Player
+---@param superFunc function
+---@vararg any
+---@return number
 function SlowPlayer.player_getDesiredSpeed(player, superFunc, ...)
     local speed = superFunc(player, ...)
-    SlowPlayer.dbgSpeed = speed
     if player.isCarryingObject and speed > player.motionInformation.maxIdleSpeed then
         speed = 1
         if player.inputInformation.runAxis > 0 then
             speed = 8
         end
     end
-    SlowPlayer.dbgOSpeed = speed
     return speed
 end
